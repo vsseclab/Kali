76c98d4e954d9d7bcd6f19eba0d498d0b75b14ad
VssecLab <91882824+vsseclab@users.noreply.github.com>
2024-03-12 19:03:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 	echo -e "_______________________________________________________________________________"
 	echo -e "                   Installing Kali Linux linux on Server"
 			
-				chmod +x .$HOME/Kalinet/files/kali
-				.$HOME/Kalinet/files/kali
+				chmod +x .$HOME/runner/work/Kalinet/files/kali
+				.$HOME/runner/work/Kalinet/files/kali
 if [ `whoami` == "root" ]; then
-else
 	echo "This script needs to be run as root!!!"
 fi
