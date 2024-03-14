ea49be14db3c38c7de32f857845981689181b07c
VssecLab <91882824+vsseclab@users.noreply.github.com>
2024-03-15 05:42:27 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-	echo -e "_______________________________________________________________________________"
-	echo -e "                   Installing Kali Linux linux on Server"
-			  wget https://raw.githubusercontent.com/vsseclab/Kalinet/master/files/kali
-				chmod +x kali
-				./kali
-if [ `whoami` == "root" ]; then
-	echo "This script needs to be run as root!!!"
-fi
